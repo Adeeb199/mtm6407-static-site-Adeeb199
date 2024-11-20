fc4f8501da9d11fc446cd159fb76223ef0bc4909
Adeeb199 <33343505+Adeeb199@users.noreply.github.com>
2024-11-20 21:22:40 +0500

    fix

---
+++ _nuxt/static/1732119363/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fmtm6407-static-site-Adeeb199\u002F_nuxt\u002Fstatic\u002F1732119363",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002Fmtm6407-static-site-Adeeb199\u002F",assetsPath:"\u002Fmtm6407-static-site-Adeeb199\u002F_nuxt\u002F",cdnURL:a}}}}(null));
